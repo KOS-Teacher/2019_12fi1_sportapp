345322010ad2e1567ab6d27bafbefbb702fd5278
MilenaGreulich <milena.greulich@web.de>
2019-12-17 12:43:54 +0100

    Klasse class

--- class/Klasse.php
+++ class/Klasse.php
@@ -3,10 +3,12 @@ class Klasse {
 
     private $Klasse_ID;
     private $Klasse_Anzahlschueler;
+    private $Klasse_Passwort
+
 
     public function __construct($Klasse_ID = null) {
         if (!empty($Klasse_ID)) {
-          //  $this->getIdStation($Klasse_ID);
+            $this->getIdKlasse($Klasse_ID);
         }
     }
 
@@ -18,28 +20,54 @@ class Klasse {
             if (xxx($q) > 0) {
                 $row = xxx($q);
                 $this->setIdKlasse($row['Klasse_ID']);
-                $this->setAnzahlSchuler($row['Klasse_Anzahlschueler']);
+                $this->setAnzahlSchueler($row['Klasse_Anzahlschueler']);
             }
             }
         }
 
-        public function updateKlasse() {
-            $sql = "UPDATE Klasse SET ";
-            $sql .= "Klasse_ID= '".$this->getKlasse()."', ";
-            $sql .= "punkte_schueler = '".$this->getPunkteSchueler()."', ";
-            $sql .= "krank_schueler = '".$this->getKrankSchueler()."', ";
-            $sql .= " WHERE id_schueler = '".$this->getIdSchueler()."' ";
-            xxx($sql);
-        }
 
-        /**
-     * Get the value of id_schueler
-     *
-     * @return mixed
-     */
+    public function addKlasse() {
+        $sql = "INSERT INTO Klasse (
+            Klasse_ID,
+            Klasse_Anzahlschueler
+        ) VALUES (
+            '".$this->getIdKlasse()."',
+            '".$this->getAnzahlSchueler()."'
+        )";
+        xxx($sql);
+        $id_schueler = xxx();
+        return $id_schueler;
+    }
+
+
+    public function editKlasse() {
+        $sql = "UPDATE Klasse SET Klasse_Anzahlschueler = '$this->getAnzahlSchueler()' WHERE Klasse_ID = '$this->getKlasseID' ";
+        xxx($sql);
+    }
+
+
+    public function deleteKlasse() {
+        $sql = "DELETE FROM Klasse WHERE Klasse_ID = '$this->getKlasseID' ";
+        xxx($sql);
+    }
+
+ 
     public function getIdKlasse()
     {
         return $this->Klasse_ID;
+    }
+
+
+    public function getAnzahlSchueler()
+    {
+        return $this->Klasse_Anzahlschueler;
+    }
+
+    public function setAnzahlSchueler($Klasse_Anzahlschueler)
+    {
+        $this->Klasse_Anzahlschueler = $Klasse_Anzahlschueler;
+
+        return $this;
     }
 
 }
