0ea715ad22db2fd6d7abd675cdd12e55347ac249
spacewar11fi1 <akira.kurusava.satushi@gmail.com>
2020-02-13 14:35:29 +0100

    committed changes

--- js/schuelerAuswertung.js
+++ js/schuelerAuswertung.js
@@ -2,23 +2,78 @@
 
           
 
-            console.log("hiiiiiiiiiiiiiii")
+            
+             var xmlhttp = new XMLHttpRequest();
 
-            var SchuelerNummer = document.getElementById("SchuelerNummer");
-            var SchuelerKlasse = document.getElementById("SchuelerKlasse");
-            var SchuelerPunkte = document.getElementById("SchuelerPunkte");
-            var KlasseRang = document.getElementById("KlasseRang");
-            var BestePunkte = document.getElementById("BestePunkte");
+             xmlhttp.onreadystatechange = function() {
+
+                 if (this.readyState == 4 && this.status == 200) {
+
+                      
+
+                      var result = this.responseText;
+
+
+                      if (result) {
+                          
+                          
+                        var jsonobj = JSON.parse(result);
+
+                        var table1 = document.getElementById("table1");
+
+                        var table2 = document.getElementById("table2");
+                        
+                        var i = 1;
+
+                        var h = 1;
+
+                        
+
+                       for(j = 0; j < jsonobj.length; j++){        
+
+                         
+
+                         if (jsonobj[j].Station_Name == "table1"){         // table1 = Auswetung Klassen
+                            
+                              table1.rows[j].cells[i].innerHTML =  jsonobj[j].Station_Name; 
+
+                              i++;
+
+                              table1.rows[j].cells[i].innerHTML =  jsonobj[j].Punkte; 
+
+                         }
+
+
+
+
+                         if (jsonobj[j].Station_Name == "table2"){        // table2 = Auswertung Sch�ler
+
+
+                              table2.rows[j].cells[h].innerHTML =  jsonobj[j].Schuler_Nummer; 
+
+                              i++;
+
+                              table2.rows[j].cells[h].innerHTML =  jsonobj[j].Station_Name; 
+
+                         }
+                           
+                                   
+                           
+                        }
+
+
+                      location.href = "admin_menu.php";    
+                 }
+            }
+          }
+
+            xmlhttp.open("GET", "../controller/getlogin_controller.php?a=" + "auswertungen");
+
+
+            //
+            xmlhttp.send();
 
             
-            
-
-          
-            SchuelerNummer.innerHTML =  result;
-            SchuelerKlasse.innerHTML =  result;
-            SchuelerPunkte.innerHTML =  result;
-            KlasseRang.innerHTML =  result;
-            BestePunkte.innerHTML =  result;
-
+        
                    
            
