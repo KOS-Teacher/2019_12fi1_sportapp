1d506621c5708f8fa3123156a689d4dc7609c5fe
SteveEv <38247760+SteveEv@users.noreply.github.com>
2020-02-17 10:07:01 +0100

    PDO gebaut

--- controller/db_controller.php
+++ controller/db_controller.php
@@ -1,10 +1,19 @@
 <?php
 
-error_reporting(0);
-$db = new mysqli('10.161.8.9', 'demo', '12FI1', 'sporttag'); // für die anderen
-#$db = new mysqli('localhost', 'root', '', 'sporttag');
-if ($db->connect_errno) {
-    die("DB Connection Error");
+$dbuser = "root";
+$dbpassword = "";
+$db = new PDO('mysql:host=localhost; dbname=sporttag; charset=utf8', $dbuser, $dbpassword);
+
+
+function getStationName($id) {
+    global $db;
+    $stmt= $db->query("SELECT * FROM station WHERE Station_ID={$id}");
+    foreach ($stmt AS $row) {
+    echo $row["Station_Name"];
+    }
 }
+
+$id = 1;
+getStationName($id);
 
 ?>
