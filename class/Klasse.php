b1dabff30bb0b66c5625a5454975d16e9cbbdc85
SteveEv <38247760+SteveEv@users.noreply.github.com>
2020-02-17 12:48:18 +0100

    klasse backend

--- class/Klasse.php
+++ class/Klasse.php
@@ -1,74 +1,32 @@
 <?php
+
 class Klasse {
 
-    private $Klasse_ID;
-    private $Klasse_Anzahlschueler;
-    private $Klasse_Passwort
+    function getKlasse() {
+        global $db;
+        $stmt= $db->query("SELECT * FROM Klasse");
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $daten[] = $row;
+        };
+        $myJSON = json_encode($daten);
+        return $myJSON;
+      }
+      function SetKlasse($col1) {
+        global $db;
+        $stmt= $db->query("INSERT INTO Klasse (`NULL`, `Klasse_Anzahlschueler`)VALUES (NULL, {$col1})");
+        
+      }
+      function UpdateKlasse($id, $col2) {
+        global $db;
+        $stmt= $db->query("UPDATE Klasse (`Klasse_ID`, `Klasse_Anzahlschueler`)VALUES ({$col2}, WHERE 'Klasse_ID' = {$id})");
+        
+      }
+      function DeleteKlasse($id) {
+        global $db;
+        $stmt= $db->query("DELETE FROM Klasse WHERE 'Klasse_ID'= {$id}");
+        var_dump($stmt);
+      }
 
 
-    public function __construct($Klasse_ID = null) {
-        if (!empty($Klasse_ID)) {
-            $this->getIdKlasse($Klasse_ID);
-        }
-    }
 
-    public function getKlasse() {
-        $Klasse_ID = $this->getIdKlasse();
-        if (!empty($Klasse_ID)) {
-            $sql = "SELECT * FROM Klasse WHERE Klasse_ID ='{$Klasse_ID}'";
-            $q = $pdo->query($sql);
-            if (xxx($q) > 0) {
-                $row = xxx($q);
-                $this->setIdKlasse($row['Klasse_ID']);
-                $this->setAnzahlSchueler($row['Klasse_Anzahlschueler']);
-            }
-            }
-        }
-
-
-    public function addKlasse() {
-        $sql = "INSERT INTO Klasse (
-            Klasse_ID,
-            Klasse_Anzahlschueler
-        ) VALUES (
-            '".$this->getIdKlasse()."',
-            '".$this->getAnzahlSchueler()."'
-        )";
-        xxx($sql);
-        $id_schueler = xxx();
-        return $id_schueler;
-    }
-
-
-    public function editKlasse() {
-        $sql = "UPDATE Klasse SET Klasse_Anzahlschueler = '$this->getAnzahlSchueler()' WHERE Klasse_ID = '$this->getKlasseID' ";
-        xxx($sql);
-    }
-
-
-    public function deleteKlasse() {
-        $sql = "DELETE FROM Klasse WHERE Klasse_ID = '$this->getKlasseID' ";
-        xxx($sql);
-    }
-
- 
-    public function getIdKlasse()
-    {
-        return $this->Klasse_ID;
-    }
-
-
-    public function getAnzahlSchueler()
-    {
-        return $this->Klasse_Anzahlschueler;
-    }
-
-    public function setAnzahlSchueler($Klasse_Anzahlschueler)
-    {
-        $this->Klasse_Anzahlschueler = $Klasse_Anzahlschueler;
-
-        return $this;
-    }
-
-}
-?>
+  }
