c67c447cd0e7227c85c8508bef0491446200419b
DemianLang <demian.lang@gmx.de>
2020-02-17 12:21:30 +0100

    schueler station class update

--- class/Schueler.php
+++ class/Schueler.php
@@ -2,159 +2,31 @@
 
 class Schueler {
 
-    private $id_schueler;
-    private $id_klasse;
-    private $punkte_schueler;
-    private $krank_schueler;
+    function getSchueler() {
+        global $db;
+        $stmt= $db->query("SELECT * FROM schueler");
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $daten[] = $row;
+        };
+        $myJSON = json_encode($daten);
+        return $myJSON;
+      }
+      function SetSchueler($col1, $col2, $col3, $col4) {
+        global $db;
+        $stmt= $db->query(INSERT INTO schueler ("`NULL`, `Klasse_ID`, `Schueler_Punkte`, `Schueler_Krank`)VALUES (NULL, {$col2}, {$col3}, {$col4}");
+        
+      }
+      function UpdateSchueler($id, $col2, $col3, $col4) {
+        global $db;
+        $stmt= $db->query("UPDATE schueler (`Klasse_ID`, `Schueler_Punkte`, `Schueler_Krank`)VALUES ({$col2}, {$col3}, {$col4}) WHERE 'Schueler_ID' = {$id}");
+        
+      }
+      function DeleteSchueler($id) {
+        global $db;
+        $stmt= $db->query("DELETE FROM schueler WHERE 'Schueler_ID'= {$id}");
+        
+      }
 
-    public function __construct($id_schueler = null) {
-        if (!empty($id_schueler)) {
-            $this->getIdStation($id_schueler);
-        }
-    }
-
-    public function getSchueler() {
-        $id_schueler = $this->getIdSchueler();
-        if (!empty($id_schueler)) {
-            $sql = "SELECT * FROM schueler WHERE id_schueler ='{$id_schueler}'";
-            $q = $pdo->query($sql);
-            if (xxx($q) > 0) {
-                $row = xxx($q);
-                $this->setIdSchueler($row['id_schueler']);
-                $this->setIdKlasse($row['id_klasse']);
-                $this->setPunkteSchuler($row['punkte_schueler']);
-                $this->setKrankSchuler($row['krank_schueler']);
-            }
-            }
-        }
-
-    public function addStation() {
-        $sql = "INSERT INTO station (
-            id_schueler,
-            id_klasse,
-            punkte_schueler,
-            krank_schueler
-        ) VALUES (
-            'null',
-            '".$this->getIdKlasse()."',
-            '".$this->getPunkteSchueler()."',
-            '".$this->getKrankSchueler()."'
-        )";
-        xxx($sql);
-        $id_schueler = xxx();
-        return $id_schueler;
-    }
-
-    public function removeSchueler($id_schueler) {
-        if (!empty($id_schueler)) {
-            $sql = "DELETE FROM schueler WHERE id_schueler = '".$id_schueler."' ";
-            xxx($sql);
-        }
-    }
-
-    public function updateSchueler() {
-        $sql = "UPDATE schueler SET ";
-        $sql .= "id_klasse = '".$this->getIdKlasse()."', ";
-        $sql .= "punkte_schueler = '".$this->getPunkteSchueler()."', ";
-        $sql .= "krank_schueler = '".$this->getKrankSchueler()."', ";
-        $sql .= " WHERE id_schueler = '".$this->getIdSchueler()."' ";
-        xxx($sql);
-    }
-
-    /**
-     * Get the value of id_schueler
-     *
-     * @return mixed
-     */
-    public function getIdSchueler()
-    {
-        return $this->id_schueler;
-    }
-
-    /**
-     * Set the value of id_schueler
-     *
-     * @param mixed id_schueler
-     *
-     * @return self
-     */
-    public function setIdSchueler($id_schueler)
-    {
-        $this->id_schueler = $id_schueler;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of id_klasse
-     *
-     * @return mixed
-     */
-    public function getIdKlasse()
-    {
-        return $this->id_klasse;
-    }
-
-    /**
-     * Set the value of id_klasse
-     *
-     * @param mixed id_klasse
-     *
-     * @return self
-     */
-    public function setIdKlasse($id_klasse)
-    {
-        $this->id_klasse = $id_klasse;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of punkte_schueler
-     *
-     * @return mixed
-     */
-    public function getPunkteStation()
-    {
-        return $this->punkte_schueler;
-    }
-
-    /**
-     * Set the value of punkte_schueler
-     *
-     * @param mixed punkte_schueler
-     *
-     * @return self
-     */
-    public function setPunkteStation($punkte_schueler)
-    {
-        $this->punkte_schueler = $punkte_schueler;
-
-        return $this;
-    }
-    /**
-     * Get the value of krank_schueler
-     *
-     * @return mixed
-     */
-    public function getOrtSation()
-    {
-        return $this->krank_schueler;
-    }
-
-    /**
-     * Set the value of krank_schueler
-     *
-     * @param mixed krank_schueler
-     *
-     * @return self
-     */
-    public function setOrtStation($krank_schueler)
-    {
-        $this->krank_schueler = $krank_schueler;
-
-        return $this;
-    }
 
 
   }
