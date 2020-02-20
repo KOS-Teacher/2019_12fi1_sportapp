55ca65eaaecbd3c76a397adb0f564dbd36265c95
SteveEv <38247760+SteveEv@users.noreply.github.com>
2020-02-20 14:23:17 +0100

    class klasse; db_controller

--- controller/db_controller.php
+++ controller/db_controller.php
@@ -2,11 +2,14 @@
 
 $dbuser = "root";
 $dbpassword = "";
-$db = new PDO('mysql:host=localhost; dbname=sporttag; charset=utf8', $dbuser, $dbpassword);
+// $db = new PDO('mysql:host=localhost; dbname=sporttag; charset=utf8', $dbuser, $dbpassword);
 
-
-
-
+try {
+    $db = new PDO('mysql:host=localhost; dbname=sporttag; charset=utf8', $dbuser, $dbpassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Verbindung fehlgeschlagen: ' . $e->getMessage();
+}
 
 
 ?>
